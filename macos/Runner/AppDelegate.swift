996764e53d069bdbbf94d4135dd09fdb521debf6
Abdeljalil Misbah <aj.msb91@gmail.com>
2025-01-25 21:46:35 +0200

    Innitial commit

---
+++ macos/Runner/AppDelegate.swift
@@ -0,0 +1,13 @@
+import Cocoa
+import FlutterMacOS
+
+@main
+class AppDelegate: FlutterAppDelegate {
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+}
